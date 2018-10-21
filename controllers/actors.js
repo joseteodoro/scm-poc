c8c1de7a98fa1fa695dc405519adf041bd305f6d
jteodoro <ubuntu@workspacetxfq0lhtlgdb0pac.che-fs-79f7bccbc5-fl8mp>
2018-10-21 23:58:34 +0000

    updating controllers

--- controllers/actors.js
+++ controllers/actors.js
@@ -1,23 +1,33 @@
+require('rootpath')()
+const { actorsModel } = require('models')
 
-var getAllActors = () => {
-	
-};
+const formatActors = (content, status) => ({status, content})
 
-var updateActor = () => {
+const formatActorsWithStatus = status => content => formatEvents(content, status)
 
-};
+const serverError = ({message, status = 500}) => Promise.reject(Object.assign(new Error(message), status))
 
-var getStreak = () => {
+const notFound = actorId => ({status: 404, message: `Could not found actor with id:${actorId}`});
 
-};
+const couldFindActor = actorId => events => events && events.length ? events : Promise.reject(notFound(actorId))
 
+var listAll = () => actorsModel.listAll()
+    .then(formatActorsWithStatus(200))
+    .catch(serverError);
+
+var updateActor = actor => actorsModel.update(actor)
+    .then(formatActorsWithStatus(200))
+    .catch(formatError(event));
+
+var getStreak = () => eventsModel.streak()
+    .then(formatActorsWithStatus(200))
+    .catch(formatError({}));
 
 module.exports = {
-	updateActor: updateActor,
-	getAllActors: getAllActors,
-	getStreak: getStreak
+	updateActor,
+	listAll,
+	getStreak
 };
-
 
 
 
--- controllers/events.js
+++ controllers/events.js
@@ -1,27 +1,44 @@
+require('rootpath')()
+const { eventsModel } = require('models')
 
-var getAllEvents = () => {
+const formatEvents = (content, status) => ({status, content})
 
-};
+const formatEventsWithStatus = status => content => formatEvents(content, status)
 
-var addEvent = () => {
+const serverError = ({message, status = 500}) => Object.assign(new Error(message), status)
 
-};
+const conflict = id => serverError({status: 409, message: `Event with id:${id} already exists.`})
 
+const formatError = ({id}) => err => err.errorCode === eventsModel.CONFLICT
+    ? Promise.reject(conflict(id))
+    : Promise.reject(serverError(err));
 
-var getByActor = () => {
+const notFound = actorId => ({status: 404, message: `Could not found actor with id:${actorId}`});
 
-};
+const couldFindActor = actorId => events => events && events.length ? events : Promise.reject(notFound(actorId))
 
+const addEvent = event => eventsModel.add(event)
+    .then(formatEventsWithStatus(201))
+    .catch(formatError(event));
+        
+const listAll = () => eventsModel.listAll()
+    .then(formatEventsWithStatus(200))
+    .catch(formatError({}));
 
-var eraseEvents = () => {
-
-};
+const getByActor = id => eventsModel.listAllByActor(id)
+    .catch(formatError({}))
+    .then(couldFindActor(id))
+    .then(formatEventsWithStatus(200));
+        
+const eraseAllEvents = () => eventsModel.truncate()
+    .then(formatEventsWithStatus(200))
+    .catch(formatError({}));
 
 module.exports = {
-	getAllEvents: getAllEvents,
-	addEvent: addEvent,
-	getByActor: getByActor,
-	eraseEvents: eraseEvents
+	listAll,
+    addEvent,
+	getByActor,
+	eraseAllEvents
 };
 
 
